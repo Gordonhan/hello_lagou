69c662163315051c4d10afb14476a65b428cbcf9
GordonHan <Gordon-Han@hotmail.com>
2017-09-04 01:56:10 +0800

    完成抓取

--- crawler/scraping_callback.py
+++ crawler/scraping_callback.py
@@ -14,13 +14,12 @@ class ScrapingCallback(object):
         tree = lxml.html.fromstring(html)
         result = []
         jobs = tree.xpath(".//div[@id='s_position_list']/ul[@class='item_con_list']/li")
-        print len(jobs)
         for job in jobs:
             company = job.xpath(".//div[@class='company_name']/a")[0].text_content()
             position = job.xpath(".//div[@class='position']//h3")[0].text_content()
             location = job.xpath(".//span[@class='add']/em")[0].text_content()
             salary = job.xpath(".//div[@class='li_b_l']//span")[0].text_content()
-            experience = job.xpath(".//div[@class='li_b_l']")[0].text_content().strip()
+            experience = job.xpath(".//div[@class='li_b_l']/span")[0].getnext().tail.strip()
             result.append({
                 'company': company,
                 'position': position,
